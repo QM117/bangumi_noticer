dfb56fdaa24bcd125cdc8aaf21c12a9cdc077ab4
Tsumugi <tsumugi52961@gmail.com>
2016-03-06 20:36:31 +0800

    更改获取更新时的策略，fix #7
     - 当连续检测到五个已存在于数据库中的条目时认为之后也全都是已经收录的，节省查询数据库的时间

--- scripts/get_bangumis.rb
+++ scripts/get_bangumis.rb
@@ -15,10 +15,16 @@ class GetBangumis
       Rails.logger.tagged("GET BANGUMIS") {Rails.logger.warn "Bad network"} and return
     end
 
+    Rails.logger.tagged("GET BANGUMIS") {Rails.logger.info "Start getting updates of bangumis"}
+
     bangumi_list = body.scan(Regexp.new(TIME + CLASSFICATION + TITLE_WITH_TAG + MAGNET_LINK_AND_SIZE))
 
+    exist_bangumis = 0
     bangumi_list.each do |bangumi|
-      if (Bangumi.find_by(title: bangumi[4], classfication: bangumi[1])).nil?
+      if Bangumi.exists?(title: bangumi[4], classfication: bangumi[1])
+        exist_bangumis += 1
+      else
+        exist_bangumis = 0
         new_bangumi = Bangumi.new(upload_at: bangumi[0], classfication: bangumi[1], fansub: bangumi[3], title: bangumi[4], magnet_link: bangumi[5], size: bangumi[6])
 
         if new_bangumi.save
@@ -28,6 +34,9 @@ class GetBangumis
         else
           Rails.logger.tagged("GET BANGUMIS") {Rails.logger.warn "Something wrong with saving a new bangumi"} and return
         end
+      end
+      if exist_bangumis == 5
+        break
       end
     end
     Rails.logger.tagged("GET BANGUMIS") {Rails.logger.info "Success getting updates of bangumis"}
